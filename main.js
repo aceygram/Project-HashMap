f259c23c66c3bbda477ccda0d3f08942b73c3fd4
Promise Okeke <ace4facebook@gmail.com>
2024-05-09 17:49:10 +0100

    created a HastSet class to create only keys to be hashed

--- main.js
+++ main.js
@@ -102,12 +102,84 @@ class HashMap{
     return result;
   }
 
-  values() {
+  entries() {
+    const buckets = this.map;
+    let result = new Array();
+    buckets.forEach(bucket => {
+      new Set(bucket.map(entry => {
+        result.push([entry.key, entry.value])
+      }));
+    })
+    return result;
+  }
+}
+
+class HashSet{
+  constructor(size){
+    this.map = new Array(size).fill(null).map(() => []);
+    this.size = size;
+  }
+
+  hash(key) {
+    let hashCode = 0;
+        
+    const primeNumber = 31;
+    for (let i = 0; i < key.length; i++) {
+        hashCode = primeNumber * hashCode + key.charCodeAt(i);
+    }
+
+    return hashCode % this.size;
+  } 
+
+  add(key) {
+    const index = this.hash(key);
+    const bucket = this.map[index];
+    if (!this.has(key)) {
+      bucket.push(key); // Append new key to the bucket if it doesn't exist
+    }
+  }
+
+
+  has(key) {
+    const index = this.hash(key);
+    const bucket = this.map[index];
+    return bucket.includes(key);
+  }
+
+  remove(key) {
+    const index = this.hash(key);
+    const bucket = this.map[index];
+    const idx = bucket.findIndex(entry => entry === key);
+    if (idx !== -1) {
+        bucket.splice(idx, 1); // Remove entry from bucket
+        return true;
+    }
+    return false;
+  }
+
+  length() {
+    const buckets = this.map;
+    let count = 0
+    
+    buckets.forEach(bucket => {
+      count += bucket.length
+    })
+    return count;
+  }
+
+  clear() {
+    const buckets = this.map;
+    buckets.forEach(bucket => {
+      bucket.length = 0;
+    })
+  }
+
+  keys() {
     const buckets = this.map;
     let result = new Array()
     buckets.forEach(bucket => {
-      new Set(bucket.map(entry => {
-        result.push(entry.value)
+      new Set(bucket.map(key => {
+        result.push(key)
       }));
     })
     return result;
@@ -115,13 +187,14 @@ class HashMap{
 }
 
 
-const HM = new HashMap(16);
+// const HM = new HashMap(16);
+const HS = new HashSet(16);
 // console.log(HM.hash('promise'));
 // console.log(HM.hash('promis'));
-HM.set('carla', 'ace');
-HM.set('carl', 'ace');
-HM.set('car', 'ace');
-HM.set('ca', 'ace');
+// HM.set('carla', 'ace');
+// HM.set('carl', 'ace');
+// HM.set('car', 'ace');
+// HM.set('ca', 'ace');
 
 // console.log(HM)
 
@@ -141,7 +214,9 @@ HM.set('ca', 'ace');
 // console.log(HM.length());
 // console.log(HM.keys());
 // console.log(HM);
-console.log(HM.values());
+// console.log(HM.values());
+// console.log(HM.entries());
+
 
 
 // HM.clear();
@@ -149,4 +224,19 @@ console.log(HM.values());
 // console.log(HM.length());
 // console.log(HM.keys());
 // console.log(HM);
+HS.add('carl');
+HS.add('cara');
+
+HS.add('carla');
+
+// console.log(HS.has('caa'));
+// console.log(HS.length());
+// HS.clear();
+// console.log(HS.length());
+
+console.log(HS.keys());
+
+// HS.remove('carla')
+// console.log(HS.has('carla'));
+
 
